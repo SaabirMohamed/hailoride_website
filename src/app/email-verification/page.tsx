ce5bcc1618f95da1d05dd8c3f22f5ab3d8f8a210
Saabir Mohamed <saabirgmohamed@gmail.com>
2023-06-13 22:11:06 +0200

    installed appwrite library and changed some html

---
+++ src/app/email-verification/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {}
+
+const EmailVerification = (props: Props) => {
+    return (
+        <main className='p-10'>
+            <h1 className='text-5xl text-amber-400'>Email Verification</h1>
+            <h2>SUCCESS</h2>
+            <p>Thank you for verifying your account</p>
+
+        </main>
+    )
+}
+
+export default EmailVerification
